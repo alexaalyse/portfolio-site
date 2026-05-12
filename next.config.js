e7c113f2795d6f6161a75c53cffb19f2564994d5
Alexa Mavrogianis <alexaalyse@gmail.com>
2026-05-11 21:03:31 -0400

    Configure GitHub Pages deployment with Next.js static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/portfolio-site',
+  assetPrefix: '/portfolio-site',
   experimental: {
     appDir: true,
   },
